983761830b4a34b204c2b4746f9f53d840bde4ae
Chion Tang <sdspeedonion@gmail.com>
2016-03-17 15:35:44 +0800

    Fix API

--- server/runapi.py
+++ server/runapi.py
@@ -27,29 +27,7 @@ def get_song_api():
 		return origin_result.text
 	song_id = json.loads(request.args['ids'])[0]
 	song_id = song_id[0:song_id.find('_')]
-	request_result = requests.get('http://music.163.com/api/song/detail/?ids=' + quote('["' + song_id + '"]') + '&id=' + song_id)
-	result_json = request_result.json()
-	song_res_id = str(result_json['songs'][0]['bMusic']['dfsId'])
-	mp3_url = "http://m%s.music.126.net/%s/%s.mp3" % (random.randrange(1, 3), encrypted_id(song_res_id), song_res_id)
-	print('Returning new result')
-	return jsonify({
-		'code' : 200,
-		'data' : [
-			{
-				'id' : song_id,
-				'url': mp3_url,
-				'br' : 64000,
-				'size':result_json['songs'][0]['bMusic']['size'],
-				'md5' : None,
-				'code': 200,
-				'expi':1200,
-				'type':'mp3',
-				'gain':0,
-				'fee':0,
-				'canExtend':False
-			}
-		]	
-	})
+	return jsonify(get_music_resource(song_id))
 
 def get_ios_response():
 	origin_result = requests.post('http://music.163.com/eapi/song/enhance/player/url', data={'params':request.form['params']})
@@ -58,29 +36,41 @@ def get_ios_response():
 		print('Returning origin result')
 		return origin_result.text
 	song_id = str(origin_result_json['data'][0]['id'])
+	return jsonify(get_music_resource(song_id))
+
+def get_music_resource(song_id):
 	request_result = requests.get('http://music.163.com/api/song/detail/?ids=' + quote('["' + song_id + '"]') + '&id=' + song_id)
 	result_json = request_result.json()
-	song_res_id = str(result_json['songs'][0]['bMusic']['dfsId'])
-	mp3_url = "http://m%s.music.126.net/%s/%s.mp3" % (random.randrange(1, 3), encrypted_id(song_res_id), song_res_id)
+	if (result_json['songs'][0]['bMusic'] != None):
+		song_res_id = str(result_json['songs'][0]['bMusic']['dfsId'])
+		file_ext = result_json['songs'][0]['bMusic']['extension']
+		file_size = result_json['songs'][0]['bMusic']['size']
+		mp3_url = "http://m%s.music.126.net/%s/%s.mp3" % (random.randrange(1, 3), encrypted_id(song_res_id), song_res_id)
+
+	else:
+		song_res_id = str(result_json['songs'][0]['audition']['dfsId'])
+		file_ext = result_json['songs'][0]['audition']['extension']
+		file_size = result_json['songs'][0]['audition']['size']
+		mp3_url = "http://m%s.music.126.net/%s/%s.%s" % (random.randrange(1, 3), encrypted_id(song_res_id), song_res_id, file_ext)
 	print('Returning new result')
-	return jsonify({
+	return {
 		'code' : 200,
 		'data' : [
 			{
 				'id' : song_id,
 				'url': mp3_url,
 				'br' : 64000,
-				'size':result_json['songs'][0]['bMusic']['size'],
+				'size': file_size,
 				'md5' : None,
 				'code': 200,
 				'expi':1200,
-				'type':'mp3',
+				'type': file_ext,
 				'gain':0,
 				'fee':0,
 				'canExtend':False
 			}
-		]	
-	})
+		]
+	}
 
 
 if __name__ == "__main__":
